cb581c495a7c74e7c345f7513ef6adef76355f7d
Mikael <62721@edu.campusvejle.dk>
2024-10-24 11:40:09 +0200

    Made the Meetings table and entity class for it

---
+++ Api/Meeting.cs
@@ -0,0 +1,16 @@
+﻿namespace Api
+{
+    public class Meeting
+    {
+       public int MeetingsId { get; set; }
+         public DateTime MeetingDate { get; set; }
+        public string MeetingName { get; set; }
+        public string MeetingSpot { get; set; }        
+        public string MeetingDescription { get; set; }
+        public string MeetingSubject { get; set; }
+
+        public int UserId { get; set; }
+        public int ContactId { get; set; }
+
+    }
+}
